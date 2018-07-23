9a27060ed8e181cc5c40472a88374f842acdcef6
Alejandra Castillo <ahle.castillo@gmail.com>
2018-07-23 02:24:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const ageIsValid = Number.isInteger();
 
 var randomNumber = Math.random(0,20);
 var randomInteger = Math.floor(randomNumber);
-var randomUserID = randomIntege
+var randomUserID = randomIntege()
